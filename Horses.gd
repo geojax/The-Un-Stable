363485b78e412575cf3c29f3c8939491a1220ab1
Alexander Su <sualexander2013@gmail.com>
2021-10-02 12:52:18 -0700

    Horses

--- Horses.gd
+++ Horses.gd
@@ -1,14 +1,10 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var horseTimer
 var Fireworks
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	Fireworks = get_node("../Fireworks")
 	Fireworks.connect("success", self, "_on_Fireworks_succeed")
@@ -18,7 +14,7 @@ func _ready():
 	horseTimer.connect("timeout", self, "_no_time")
 	add_child(horseTimer)
 	horseTimer.start()
-	pass # Replace with function body.
+	pass 
 
 func _on_Fireworks_succeed():
 	horseTimer.start(5)
