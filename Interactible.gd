d4e73efe93df6ccb8d57a3e1102ea54af6afac4d
geojax <50026101+geojax@users.noreply.github.com>
2021-10-02 12:06:31 -0700

    success, failure signals

--- Interactible.gd
+++ Interactible.gd
@@ -6,11 +6,13 @@ extends Node2D
 # var b = "text"
 onready var typingGame := get_node("TypingGame")
 signal failure
+signal success
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Area2D.connect("area_entered", self, "_on_Area2D_area_entered")
 	$Area2D.connect("area_exited", self, "_on_Area2D_area_exited")
 	typingGame.connect("failure", self, "_on_TypingGame_fail")
+	typingGame.connect("success", self, "_on_TypingGame_succeed")
 	typingGame.visible = false
 	pass # Replace with function body.
 
@@ -33,3 +35,6 @@ func _on_Area2D_area_exited(area):
 
 func _on_TypingGame_fail():
 	emit_signal("failure")
+	
+func _on_TypingGame_succeed():
+	emit_signal("success")
--- Player.gd
+++ Player.gd
@@ -5,13 +5,30 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var numLives = 3
-
+var numMedbaySuccesses = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	$Fireworks.connect("failure", self, "_on_Fireworks_fail")
+	$Medbay.connect("success", self, "_on_Medbay_success")
+	$Medbay.connect("failure", self, "_on_Medbay_fail")
 	pass # Replace with function body.
 
-
+func _on_Fireworks_fail():
+	numLives -= 1
+	print(numLives)
+	pass
+	
+func _on_Medbay_success():
+	numMedbaySuccesses += 1
+	if numMedbaySuccesses >= 3:
+		numLives += 1
+		numMedbaySuccesses = 0
+	pass
+	
+func _on_Medbay_fail():
+	numMedbaySuccesses = 0
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
