3253a557beaf9ea4b5668f9d3554439c7c6fc6c3
Alexander Su <sualexander2013@gmail.com>
2021-10-03 20:08:40 -0700

    Portal Potty

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -1,33 +1,52 @@
 extends "Interactible.gd"
 
 const MAX_STABILITY = 5
+const COOLDOWN = 10
 var stability := 4.9
 var portalProgress
+var onCooldown = false
 onready var animSprite := get_node("AnimatedSprite")
+var portalTimer
 
 func _ready():
 #	portalProgress = get_node("PortalProgress")	
 	typingGame.connect("success", self, "stabilizeMore")
 	#typingGame.connect("failure", self, "_on_TypingGame_fail")
+	portalTimer = Timer.new()
+	portalTimer.set_one_shot(true)
+	portalTimer.connect("timeout", self, "_no_time")
+	add_child(portalTimer)
 	pass 
 	
 func stabilizeMore():
-	stability += 1
+	if not onCooldown:
+		stability += 1
 	print("stabilizing")
-	if stability == 5:
-		stability = 0 
+	if stability >= MAX_STABILITY:
+		stability = 0
+		portalTimer.start(COOLDOWN)
+		onCooldown = true
+		
 	print($PortalProgress.value)
 		
 func _on_TypingGame_fail():
 	stability = 0
 	pass
 	
+func _no_time():
+	onCooldown = false
+	
+	
 func _process(delta):
-	if stability > 0:
+	if stability > 0 and (not onCooldown):
 		stability -= delta * .05
-		
+	if onCooldown:
+		$AnimationPlayer.play("Cooldown")
 	$PortalProgress.value = (float(stability) / MAX_STABILITY) * 100
-	animSprite.speed_scale = 6 - stability
+	if not onCooldown:
+		animSprite.speed_scale = 6 - stability
+	else:
+		animSprite.speed_scale = 0.25
 	pass
 
 
